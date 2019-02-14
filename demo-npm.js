df55c02c9edbd4e3b4d9da83e18442b23b42e42c
Sibsankar Bera <sibsankarb4@gmail.com>
2019-02-14 15:01:37 +0530

    Delete demo-npm.js

--- demo-npm.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-var uc = require('upper-case');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(uc("Hello World!"));
-  res.end();
-}).listen(8080);
