b55072bd973960353fc8428be2c13b2852aa01f5
Sibsankar Bera <sibsankarb4@gmail.com>
2019-02-14 14:59:09 +0530

    Delete demo-myModule.js

--- demo-myModule.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var dt = require('./myCustomModule');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("The date and time are currently: " + dt.myDateTime());
-  res.end();
-}).listen(8080);
