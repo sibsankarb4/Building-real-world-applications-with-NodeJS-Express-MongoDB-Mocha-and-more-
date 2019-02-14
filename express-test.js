69f21fcbe30231b088faf00e3ca189b2ecfe015f
Sibsankar Bera <sibsankarb4@gmail.com>
2019-02-14 15:00:07 +0530

    Update express-test.js

--- express-test.js
+++ express-test.js
@@ -4,5 +4,5 @@ app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
 app.get("/url", (req, res, next) => {
- res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+ res.json(["Sib","Sankar","Bera","Java","NodeJS"]);
 });
